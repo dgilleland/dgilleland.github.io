7875d78df93ef97e1dd01bc90b798bc1ef5baa25
Dan Gilleland <dgilleland@nait.ca>
2018-07-13 06:51:03 -0600

    Add markdown extensions

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,19 @@
 module.exports = {
     title: 'Daniel Gilleland',
-    description: 'Instructor Blog'
+    description: 'Instructor Blog',
+    markdown: {
+        config: md => {
+            md.set({ breaks: true })
+            // md.use(require('markdown-it-imsize'))
+            // md.use(require('markdown-it-mermaid').default) // leave default options
+            // md.use(require('markdown-it-checkbox'))
+            // md.use(require('markdown-it-kbd'))
+            // md.use(require('markdown-it-deflist'))
+            // md.use(require('markdown-it-abbr'))
+            // md.use(require('markdown-it-mark'))
+            // md.use(require('markdown-it-footnote'))
+            md.use(require('markdown-it-sub'))
+            md.use(require('markdown-it-sup'))
+              }
+    }
   }
