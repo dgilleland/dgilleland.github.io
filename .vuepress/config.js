55095289916f53e2a46b8e1d4a104930555ad10f
Dan Gilleland <dgilleland@nait.ca>
2018-09-04 09:04:51 -0600

    Add link to this term's code

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -34,6 +34,7 @@ module.exports = {
             { text: 'Teaching Schedule', link: '/new/' },
             { text: 'DMIT-1508', link: 'https://DMIT-1508.github.io' },
             { text: 'DMIT-2018', link: 'https://DMIT-2018.github.io' },
+            { text: 'Sep 2018 Errata', link: 'https://dgilleland.github.io/2018-Sep-In-Class/'}
         ],
         sidebar: [
             '/new/',
