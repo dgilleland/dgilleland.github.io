5cb5d89975b5b0adbc29afa13448ca9025d8d023
Dan Gilleland <dgilleland@nait.ca>
2024-04-19 08:43:09 -0600

    Install Expressive Code + blogged about it

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,28 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-
 import icon from "astro-icon";
+
+import expressiveCode from "astro-expressive-code";
+import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
+import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-sphere-demo.vercel.app",
-  integrations: [mdx(), sitemap(), solidJs(), tailwind({
+  integrations: [expressiveCode({
+    plugins: [pluginLineNumbers(), pluginCollapsibleSections()],
+    defaultProps: {
+      // Disable line numbers by default
+      showLineNumbers: false,
+      // But enable line numbers for certain languages
+      overridesByLang: {
+        'js,ts,html': {
+          showLineNumbers: true,
+        },
+      },
+    },
+  }), mdx(), sitemap(), solidJs(), tailwind({
     applyBaseStyles: false
   }), icon()]
 });
